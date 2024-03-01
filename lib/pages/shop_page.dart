4b03255d5c560bb2fde99b221c90b72d922a65a0
Paweł <pawel_gnat@o2.pl>
2024-03-01 18:32:55 +0100

    feat: shop page

---
+++ lib/pages/shop_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ShopPage extends StatefulWidget {
+  const ShopPage({super.key});
+
+  @override
+  State<ShopPage> createState() => _ShopPageState();
+}
+
+class _ShopPageState extends State<ShopPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+            padding: const EdgeInsets.all(12),
+            margin: const EdgeInsets.symmetric(horizontal: 25),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text(
+                    'Search',
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                  Icon(Icons.search, color: Colors.grey)
+                ]))
+      ],
+    );
+  }
+}
